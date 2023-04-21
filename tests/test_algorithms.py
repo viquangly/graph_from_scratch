177c813f4e9c999e9a7769e0098e40ecf30cf0fe
ViLy <viquangly@gmail.com>
2023-04-21 18:40:13 -0400

    Initial commit of test_algorithms.py

---
+++ tests/test_algorithms.py
@@ -0,0 +1,21 @@
+
+from algorithms.djikstra import djikstra
+from algorithms.floyd_warshall import floyd_warshall
+import paths.shortest_path as sp
+from tests import datasets as ds
+
+
+def test_djisktra():
+    graph = ds.weighted_path_graph()
+    distance_dict, prev_dict = djikstra(graph, 'a')
+    shortest_path, distance = sp._shortest_path(distance_dict, prev_dict, 'a', 'd')
+    assert shortest_path == list('abefgd')
+    assert distance == 5
+
+
+def test_floyd_warshall():
+    graph = ds.weighted_path_graph()
+    distance_dict, prev_dict = floyd_warshall(graph)
+    shortest_path, distance = sp._shortest_path(distance_dict, prev_dict, 'a', 'd')
+    assert shortest_path == list('abefgd')
+    assert distance == 5
